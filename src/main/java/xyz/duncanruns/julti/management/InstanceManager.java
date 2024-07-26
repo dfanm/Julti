67d69f79cf9ec70f2e48e03056d66fb9508487f5
dfanm <dfanm26@gmail.com>
2024-07-26 05:57:19 -0700

    Update InstanceManager.java

--- src/main/java/xyz/duncanruns/julti/management/InstanceManager.java
+++ src/main/java/xyz/duncanruns/julti/management/InstanceManager.java
@@ -6,20 +6,16 @@ import xyz.duncanruns.julti.JultiOptions;
 import xyz.duncanruns.julti.instance.InstanceState;
 import xyz.duncanruns.julti.instance.MinecraftInstance;
 import xyz.duncanruns.julti.plugin.PluginEvents;
-import xyz.duncanruns.julti.resetting.ResetHelper;
 import xyz.duncanruns.julti.util.DoAllFastUtil;
 import xyz.duncanruns.julti.util.MouseUtil;
 import xyz.duncanruns.julti.util.SleepBGUtil;
 import xyz.duncanruns.julti.win32.User32;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Collectors;
 
 import static xyz.duncanruns.julti.util.SleepUtil.sleep;
 
@@ -27,7 +23,6 @@ public final class InstanceManager {
     private static final InstanceManager INSTANCE = new InstanceManager();
     private final ArrayList<MinecraftInstance> instances = new ArrayList<>();
 
-    private boolean utilityMode = false;
     private boolean instancesMissing = true;
 
     private InstanceManager() {
@@ -42,7 +37,6 @@ public final class InstanceManager {
     }
 
     public void onOptionsLoad() {
-        this.loadInstances();
         this.checkOpenedInstances();
     }
 
@@ -86,7 +80,6 @@ public final class InstanceManager {
             sleep(50);
         });
         OBSStateManager.getOBSStateManager().tryOutputLSInfo();
-        ResetHelper.getManager().reload();
         PluginEvents.RunnableEventType.ALL_INSTANCES_FOUND.runAll();
     }
 
@@ -103,33 +96,8 @@ public final class InstanceManager {
         return this.instancesMissing;
     }
 
-    public void runChecks() {
-        if (this.utilityMode) {
-            this.runChecksUtility();
-        } else {
-            this.runChecksRegular();
-        }
-    }
-
     public void tick(long cycles) {
-        if (JultiOptions.getJultiOptions().utilityMode != this.utilityMode) {
-            this.reloadUtilityMode();
-        }
-        if (this.utilityMode) {
-            this.tickUtility(cycles);
-        } else {
-            this.tickRegular(cycles);
-        }
-    }
-
-    private void reloadUtilityMode() {
-        this.utilityMode = JultiOptions.getJultiOptions().utilityMode;
-        if (this.utilityMode) {
-            this.instances.clear();
-        } else {
-            this.loadInstances();
-        }
-        this.runChecks();
+        this.tickUtility(cycles);
     }
 
     private void tickUtility(long cycles) {
@@ -145,32 +113,9 @@ public final class InstanceManager {
         InstanceChecker.getInstanceChecker().getAllOpenedInstances().stream().filter(i -> !this.instances.contains(i)).forEach(i -> {
             i.discoverInformation();
             this.instances.add(i);
-            if (JultiOptions.getJultiOptions().utilityModeUsePlayingSettings) {
-                i.ensurePlayingWindowState();
-            }
+            i.ensurePlayingWindowState();
         });
         this.checkForWindowRename();
-    }
-
-    private void tickRegular(long cycles) {
-        this.instances.forEach(MinecraftInstance::checkWindowMissing);
-        if (cycles % 5000 == 0) {
-            this.runChecksRegular();
-        }
-    }
-
-    private void runChecksRegular() {
-        if (this.checkInstancesMarkedMissing()) {
-            this.checkOpenedInstances();
-            return;
-        }
-        this.checkForWindowRename();
-    }
-
-    private void checkForWindowRename() {
-        if (this.instances.stream().anyMatch(instance -> instance.getStateTracker().isCurrentState(InstanceState.TITLE))) {
-            this.renameWindows();
-        }
     }
 
     public void tickInstances() {
@@ -218,38 +163,6 @@ public final class InstanceManager {
             }
         }
     }
-
-    public void removeInstance(MinecraftInstance instance) {
-        if (!this.instances.contains(instance)) {
-            Julti.log(Level.WARN, "An instance not found in the instances list was requested to be removed! Likely a GUI desync.");
-        }
-        this.instances.remove(instance);
-        this.saveInstances();
-        ResetHelper.getManager().reload();
-    }
-
-    public void saveInstances() {
-        JultiOptions.getJultiOptions().instancePaths = this.instances.stream().map(instance -> instance.getPath().toString()).collect(Collectors.toList());
-    }
-
-    public void loadInstances() {
-        this.instances.clear();
-        JultiOptions.getJultiOptions().instancePaths.forEach(pathString -> this.instances.add(new MinecraftInstance(Paths.get(pathString))));
-    }
-
-    public void redetectInstances() {
-        Julti.log(Level.DEBUG, "Redetect running...");
-        this.instances.clear();
-        Julti.log(Level.DEBUG, "Cleared instances");
-        this.instances.addAll(InstanceChecker.getInstanceChecker().getAllOpenedInstances());
-        Julti.log(Level.DEBUG, "Added instances");
-        this.instances.sort(Comparator.comparingInt(MinecraftInstance::getNameSortingNum));
-        Julti.log(Level.DEBUG, "Sorted instances");
-        this.saveInstances();
-        Julti.log(Level.DEBUG, "Saved instances");
-        this.onAllInstancesFound();
-    }
-
     public MinecraftInstance getMatchingInstance(MinecraftInstance instance) {
         if (instance == null) {
             return null;
@@ -261,4 +174,8 @@ public final class InstanceManager {
         }
         return instance;
     }
-}
+    private void checkForWindowRename() {
+        if (this.instances.stream().anyMatch(instance -> instance.getStateTracker().isCurrentState(InstanceState.TITLE))) {
+            this.renameWindows();
+        }
+    }}
